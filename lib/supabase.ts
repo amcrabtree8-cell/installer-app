9e64d64fc2710ba67d26e2c597862ef49052b04c
amcrabtree8-cell <amcrabtree8@gmail.com>
2026-06-01 22:24:31 -0500

    Update Supabase URL in supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://ikluvcbhgwsjkhgmqzo.supabase.co',
+ 'https://lkluvcbhgwgsjkhgmqzo.supabase.co',
   'sb_publishable_njgndx1Qy-3s6i0Sk3fDyg_fm7NCIh8'
 )
